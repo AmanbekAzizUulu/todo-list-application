573dcdc69d88a68742fd48f7312a1c00cdd039d4
Amanbek <amanbekazizuulu@gmail.com>
2025-02-09 04:30:05 +0600

    done refactoring

--- src/main/java/com/dandaevit/edu/controllers/AddUserServlet.java
+++ src/main/java/com/dandaevit/edu/controllers/AddUserServlet.java
@@ -2,9 +2,10 @@ package com.dandaevit.edu.controllers;
 
 import java.io.IOException;
 
-import com.dandaevit.edu.dto.UserDTO;
+import com.dandaevit.edu.model.User;
 import com.dandaevit.edu.utils.jsp_utils.JSPUtils;
 
+import at.favre.lib.crypto.bcrypt.BCrypt;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
@@ -15,7 +16,7 @@ public class AddUserServlet extends HttpServlet {
 
 	@Override
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
-		// TODO validate and add user to databas
+		// TODO validate and add user to database
 
 		var firstName = req.getParameter("firstName");
 		var lastName = req.getParameter("lastName");
@@ -25,11 +26,17 @@ public class AddUserServlet extends HttpServlet {
 		System.out.println(lastName);
 		System.out.println(email);
 
-		var userDTO = new UserDTO(firstName, lastName, email);
+		var user = User.builder()
+					   .id(1001)
+		               .firstName("Aibek")
+					   .lastName("Dandaev")
+					   .email("aibekdandaev@gmail.com")
+					   .password(BCrypt.withDefaults().hashToString(12, "password_1234567890".toCharArray()))
+					   .build();
 
-		System.out.println(userDTO);
-		
-		req.getSession().setAttribute("user", userDTO);
+		System.out.println(user);
+
+		req.getSession().setAttribute("user", user);
 
 		var userDTO_1 = req.getAttribute("user");
 		System.out.println(userDTO_1);
